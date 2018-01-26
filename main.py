93105e39e80584c400c79fe5676de9ce22bf5706
Alex Gunning <alexandergunning@gmail.com>
2018-01-25 23:44:21 -0600

    Added cors header and getBeerByIdFunction

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from secrets import token_hex
-from bson import ObjectId
+from flask_cors import CORS
+from bson.objectid import ObjectId
 from flask import Flask, jsonify, request, abort
 from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+CORS(app)
 bcrypt = Bcrypt(app)
 
 app.config['MONGO_DBNAME'] = 'beer'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/beer'
 
 mongo = PyMongo(app)
+
+##TODO throw errors in getting user ids and tokens and catch them in auth routes to abort(401) instead of in repo functions
 
 def generate_token():
     return token_hex(16)
@@ -29,16 +33,27 @@ def get_user_token(username, password):
 
 def get_user_id_from_request(req):
     token = req.headers.get('token')
+    if not token:
+        token = 'de32ae4a14bc52375f8f725b5874cd43'
     user_id = get_user_id(token)
     if not user_id:
         abort(401)
     return user_id
 
 def map_beers(beers):
+    '''jsonify doesn't convert object ids to str so find all object ids and convert to str'''
     for beer in beers:
-        beer['_id'] = str(beer['_id'])
-        beer['user'] = str(beer['user'])
+        for key, val in beer.items():
+            if val.__class__.__name__ == "ObjectId":
+                beer[key] = str(val)
     return beers
+#TODO figure out how to combine these two functions
+def map_beer(beer):
+    '''jsonify doesn't convert object ids to str so find all object ids and convert to str'''
+    for key, val in beer.items():
+        if val.__class__.__name__ == "ObjectId":
+            beer[key] = str(val)
+    return beer
 
 
 @app.route('/newUser', methods=['POST'])
@@ -63,18 +78,25 @@ def login():
 
 @app.route('/addBeer', methods=['POST'])
 def add_beer():
-    user = get_user_id_from_request(request)
+    user_id = get_user_id_from_request(request)
     name = request.args.get('name')
     brewer = request.args.get('brewer')
     abv = request.args.get('abv')
-    beer = mongo.db.beers.insert_one({"name": name, "brewer": brewer, "abv": abv, "user": ObjectId(user)})
+    beer = mongo.db.beers.insert_one({"name": name, "brewer": brewer, "abv": abv, "user": ObjectId(user_id)})
     return jsonify({"_id" : str(beer.inserted_id)})
 
 @app.route('/getBeers')
 def get_beers():
-    user = get_user_id_from_request(request)
-    beer = mongo.db.beers.find({"user": ObjectId(user)})
-    mapped_beer = map_beers(list(beer))
+    user_id = get_user_id_from_request(request)
+    beers = mongo.db.beers.find({"user": ObjectId(user_id)})
+    mapped_beer = map_beers(list(beers))
+    return jsonify(mapped_beer)
+
+@app.route('/getBeerById/<string:beer_id>')
+def get_beer_by_id(beer_id):
+    # user_id = get_user_id_from_request(request)
+    beer = mongo.db.beers.find({"_id": ObjectId(beer_id)})
+    mapped_beer = map_beer(beer)
     return jsonify(mapped_beer)
 
 if __name__ == '__main__':
