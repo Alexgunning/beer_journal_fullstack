fbaa2a6a9deef49c4d0477b717def641592a0035
Alex Gunning <alexandergunning@gmail.com>
2018-02-07 17:32:38 -0600

    added header component

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const headerStyle = {
+  width: '30%',
+  margin: '0 auto',
+  'font-size': '4vw',
+  'text-align': 'center'
+
+}
+
+const Header = () => (
+  <div style={headerStyle}>
+    Beer Journal
+  </div>
+)
+
+export default Header
