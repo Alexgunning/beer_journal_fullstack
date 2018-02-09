a33e50a4582a67806fab305998e3dd96a218f749
Alex Gunning <alexandergunning@gmail.com>
2018-02-08 20:30:51 -0600

    Changes for beer list styling

--- src/containers/beerList.js
+++ src/containers/beerList.js
@@ -3,19 +3,33 @@ import { connect } from 'react-redux'
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import { List, Avatar, Icon } from 'antd';
 
 import Beer from '../components/beer'
 import { fetchBeerListIfNeeded } from '../actions/fetchBeerList'
 import { requestBeer, fetchBeerIfNeeded, newBeer } from '../actions/selectedBeer'
 import { postBeer } from '../actions/postBeer'
 
-// import Button from 'antd/lib/button';
-// import '../App.css';
-
-const tableStyle = {
-  float: 'left',
-  width: '350px'
+const listStyle = {
+  width: "50%",
+  margin: "0 auto",
+  "padding-bottom": "30px"
 };
+
+const listItemStyle = {
+  background: "#fbfbfb",
+  border: "1px solid #d9d9d9",
+  "border-radius": "6px", 
+  "padding": "12px",
+  "margin": "6px"
+};
+
+const IconText = ({ type, text }) => (
+  <span>
+    <Icon type={type} style={{ marginRight: 8 }} />
+    {text}
+  </span>
+);
 
 class BeerList extends Component {
 
@@ -24,7 +38,8 @@ class BeerList extends Component {
       _id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       brewer: PropTypes.string.isRequired,
-      abv: PropTypes.number.isRequired
+      abv: PropTypes.number.isRequired,
+      img: PropTypes.string.isRequired
     }).isRequired).isRequired,
     dispatch: PropTypes.func.isRequired
   }
@@ -36,30 +51,30 @@ class BeerList extends Component {
 
   render() {
     return (
-      <div style={tableStyle}>
-        <table>
-          <tr>
-            <th>name</th>
-            <th>brewer</th>
-            <th>abv</th>
-          </tr>
-          {this.props.beers.map(beer =>
-            <Beer
+      <div style={listStyle}>
+        <List
+          itemLayout="vertical"
+          size="large"
+          dataSource={this.props.beers}
+          renderItem={beer => (
+            <List.Item
               key={beer._id}
-              {...beer}
-              dispatch={this.props.dispatch}
-              requestBeer={this.props.requestBeer}
-            />
+              style={listItemStyle}
+              actions={[]}
+              extra={<img width={67} height={180} alt="logo" src={beer.img} />}
+            >
+              <List.Item.Meta
+                title={<a href="http://daringfireball.net">{beer.name}</a>}
+                description={beer.brewer}
+                bordered={true}
+              />
+              {beer.abv}% ABV
+            </List.Item>
           )}
-        </table>
-        <button onClick={() => {this.props.dispatch(this.props.newBeer()) }}>
-            Add New Beer
-        </button>
-        <button onClick={() => {this.props.dispatch(this.props.postBeer({name: "High Life", brewer: "Miller High Life", abv: "4.2"})) }}>
-            Post New Beer
-        </button>
+        />
       </div>
-    )
+    );
+
   }
 }
 
