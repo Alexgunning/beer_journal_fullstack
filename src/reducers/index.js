1213d7de535300c579827fe7ad1a27e3e844561c
Alex Gunning <alexandergunning@gmail.com>
2018-02-11 22:58:57 -0600

    cleaned up selected beer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -36,38 +36,15 @@ function allBeers(state = { isFetching: false, beers: [] }, action) {
   }
 }
 
-let data = {
-    _id: "alex",
-    beer: 'Victory at Sea',
-    brewer: 'Ballast Point',
-    abv: '6.8',
-    description : "sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morb", }
 
-let data1 = {
-    _id: "ball",
-    beer: 'Sierra Nevada',
-    brewer: 'Torpedo',
-    abv: '7.6',
-    description : "sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morb",
-  }
-
-let data2 = {
-    _id: 'bfdalj',
-    beer: 'divided sky',
-    brewer: '4 hands',
-    abv: '9.6',
-    description : "sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morb",
-  }
-
-function selectedBeer(state = data, action) {
+function selectedBeer(state = {}, action) {
   switch (action.type) {
     case REQUEST_SELECTED_BEER:
-      return data1
+      return {};
     case NEW_BEER:
-      //TODO figure out if we want to have the action set the id or the API
-      return data2
+      return {};
     case RECEIVE_SELECTED_BEER:
-      return action.beer
+      return action.beer;
     default:
       return state
 }
