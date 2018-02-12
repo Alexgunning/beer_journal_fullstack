8324fca0c77553b7e37bc1b35f2292ab74b9774f
Alex Gunning <alexandergunning@gmail.com>
2018-02-12 00:54:36 -0600

    Added beer form contiainer for handling new and editing beers

---
+++ src/containers/beerForm.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Field, reduxForm } from 'redux-form'
+import { requestBeer, fetchBeerIfNeeded, newBeer } from '../actions/selectedBeer'
+import AntForm from '../components/antForm.js'
+
+const listStyle = {
+  width: "20%",
+  margin: "0 auto"
+};
+
+class BeerForm extends Component {
+
+  componentDidMount() {
+    let { fetchBeer } = this.props
+    console.log(this.props.match);
+    if (this.props.match.path == "/new")
+      newBeer()
+    else {
+      let beerId = this.props.match.params.id;
+      fetchBeer(beerId)
+    }
+  }
+  render() {
+    const { selectedBeer, handleSubmit, load, pristine, reset, submitting } = this.props
+    if (selectedBeer.loading){
+      return (
+        <div>
+          loading
+        </div>
+      )
+    }
+    else {
+      return(
+        <AntForm initialValues={selectedBeer.beer}/>
+      )
+    }
+  }
+}
+
+
+// You have to connect() to any reducers that you wish to connect to yourself
+BeerForm = connect(
+  state => ({
+    selectedBeer: state.selectedBeer
+  }),
+  dispatch => ({
+    fetchBeer: (beerId) =>  dispatch(fetchBeerIfNeeded(beerId)),
+    newBeer: () => dispatch(newBeer)
+  }) // bind account loading action creator
+)(BeerForm)
+
+export default BeerForm
