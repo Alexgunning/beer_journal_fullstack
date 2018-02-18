1afa42f696e815934dde3f773b7eab967339a377
Alex Gunning <alexandergunning@gmail.com>
2018-02-17 22:50:12 -0600

    Added some actions for checking local token and verifying it remotley

--- src/actions/login.js
+++ src/actions/login.js
@@ -3,6 +3,11 @@ import axios from 'axios';
 export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS'
 export const LOGIN_USER_FAILURE = 'LOGIN_USER_FAILURE'
 export const LOGIN_USER_REQUEST = 'LOGIN_USER_REQUEST'
+export const CHECK_LOCAL_TOKEN_SUCCESS = 'CHECK_LOCAL_TOKEN_SUCCESS'
+export const CHECK_LOCAL_TOKEN_FAILURE = 'CHECK_LOCAL_TOKEN_FAILURE'
+export const VERIFY_TOKEN_SUCCESS = 'VERIFY_TOKEN_SUCCESS'
+export const VERIFY_TOKEN_FAILURE = 'VERIFY_TOKEN_FAILURE'
+export const VERIFY_TOKEN_REQUEST = 'VERIFY_TOKEN_REQUEST'
 export const LOGOUT_USER = 'LOGOUT_USER'
 export const REGISTER_USER_FAILURE = 'REGISTER_USER_FAILURE'
 export const REGISTER_USER_REQUEST = 'REGISTER_USER_REQUEST'
@@ -104,15 +109,31 @@ export function loginUser(email, password) {
     };
 }
 
-
-export function registerUserRequest() {
+export function checkTokenRequest() {
+    token = localStorage.getItem('token');
     return {
-        type: REGISTER_USER_REQUEST,
+        type: CHECK_LO_TOKEN_REQUEST,
     };
 }
 
-export function registerUserSuccess(token) {
-    localStorage.setItem('token', token);
+export function checkLocalToken(email, password) {
+  let token = localStorage.getItem('token');
+  if (token != null) {
+    return function (dispatch) {
+      dispatch(checkLocalTokenSuccess(token));
+      return create_user(email, password)
+    }
+  }
+  else {
+    return function (dispatch) {
+      dispatch(checkLocalTokenFailure());
+      return create_user(email, password)
+    }
+  }
+}
+
+export function checkLocalTokenSuccess(token) {
+    token = localStorage.getItem('token');
     return {
         type: REGISTER_USER_SUCCESS,
         payload: {
@@ -121,44 +142,67 @@ export function registerUserSuccess(token) {
     };
 }
 
+export function checkLocalTokenFailure() {
+  return {
+    type: CHECK_TOKEN_FAILURE
+  };
+}
+
+
+export function registerUserRequest() {
+  return {
+    type: REGISTER_USER_REQUEST,
+  };
+}
+
+export function registerUserSuccess(token) {
+  localStorage.setItem('token', token);
+  return {
+    type: REGISTER_USER_SUCCESS,
+    payload: {
+      token,
+    },
+  };
+}
+
 export function registerUserFailure(error) {
-    localStorage.removeItem('token');
-    return {
-        type: REGISTER_USER_FAILURE,
-        payload: {
-            status: error.response.status,
-            statusText: error.response.statusText,
-        },
-    };
+  localStorage.removeItem('token');
+  return {
+    type: REGISTER_USER_FAILURE,
+    payload: {
+      status: error.response.status,
+      statusText: error.response.statusText,
+    },
+  };
 }
 
 export function registerUser(email, password) {
-    return function (dispatch) {
-        dispatch(registerUserRequest());
-        return create_user(email, password)
-            .then(parseJSON)
-            .then(response => {
-                try {
-                    dispatch(registerUserSuccess(response.token));
-                    // browserHistory.push('/main');
-                } catch (e) {
-                    dispatch(registerUserFailure({
-                        response: {
-                            status: 403,
-                            statusText: 'Invalid token',
-                        },
-                    }));
-                }
-            })
-            .catch(error => {
-                dispatch(registerUserFailure({
-                    response: {
-                        status: 403,
-                        statusText: 'User with that email already exists',
-                    },
-                }
-                ));
-            });
-    };
+  return function (dispatch) {
+    dispatch(registerUserRequest());
+    return create_user(email, password)
+      .then(parseJSON)
+      .then(response => {
+        try {
+          dispatch(registerUserSuccess(response.token));
+          // browserHistory.push('/main');
+        } catch (e) {
+          dispatch(registerUserFailure({
+            response: {
+              status: 403,
+              statusText: 'Invalid token',
+            },
+          }));
+        }
+      })
+      .catch(error => {
+        dispatch(registerUserFailure({
+          response: {
+            status: 403,
+            statusText: 'User with that email already exists',
+          },
+        }
+        ));
+      });
+  };
 }
 
