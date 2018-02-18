14d7f5361f4eeec183f47ff813c71d56ed1435e4
Alex Gunning <alexandergunning@gmail.com>
2018-02-18 16:27:48 -0600

    update notAuthenticated componenet

--- src/components/notAuthenticatedComponent.js
+++ src/components/notAuthenticatedComponent.js
@@ -1,94 +1,52 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-// import { browserHistory } from 'react-router';
 import * as actionCreators from '../actions/login';
-
-function mapStateToProps(state) {
-    return {
-        token: state.auth.token,
-        userName: state.auth.userName,
-        isAuthenticated: state.auth.isAuthenticated,
-    };
-}
-
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators(actionCreators, dispatch);
-}
-
+import { Route, Redirect }from "react-router-dom";
 
 export function requireNoAuthentication(Component) {
-
-    class notAuthenticatedComponent extends React.Component {
-
-        constructor(props) {
-            super(props);
-            this.state = {
-                loaded: false,
-            };
-        }
-
-        componentWillMount() {
-            this.checkAuth();
-        }
-
-        componentWillReceiveProps(nextProps) {
-            this.checkAuth(nextProps);
-        }
-
-        checkAuth(props = this.props) {
-            if (props.isAuthenticated) {
-                // browserHistory.push('/main');
-
-            } else {
-                const token = localStorage.getItem('token');
-                if (token) {
-                    fetch('/api/is_token_valid', {
-                        method: 'post',
-                        credentials: 'include',
-                        headers: {
-                            'Accept': 'application/json', // eslint-disable-line quote-props
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({ token }),
-                    })
-                        .then(res => {
-                            if (res.status === 200) {
-                                this.props.loginUserSuccess(token);
-                                // browserHistory.push('/main');
-
-                            } else {
-                                this.setState({
-                                    loaded: true,
-                                });
-                            }
-                        });
-                } else {
-                    this.setState({
-                        loaded: true,
-                    });
-                }
-            }
-        }
-
-        render() {
-            return (
-                <div>
-                    {!this.props.isAuthenticated && this.state.loaded
-                        ? <Component {...this.props} />
-                        : null
-                    }
-                </div>
-            );
-
-        }
+  class AuthenticatedComponent extends React.Component {
+    componentWillMount() {
+      const { dispatch, checkLocalToken, isAuthenticated, isAuthenticating } = this.props
+      if (!isAuthenticated)
+        checkLocalToken()
     }
 
-    notAuthenticatedComponent.propTypes = {
-        loginUserSuccess: React.PropTypes.func,
-        isAuthenticated: React.PropTypes.bool,
+    render() {
+    let returnAddress = this.props.location.state ? this.props.location.state.from : "/";
+      return (
+        <div>
+          <Route
+            render={props =>
+                !this.props.isAuthenticated  ? (
+                  <Component {...props} />
+                ) : (
+                  <Redirect
+                    to={{
+                      pathname:"/"
+                    }}
+                  />
+                )
+            }
+          />
+        </div>
+      );
+
+    }
+  }
+
+  function mapStateToProps(state) {
+    return {
+      token: state.auth.token,
+      email: state.auth.email,
+      isAuthenticated: state.auth.isAuthenticated,
     };
+  }
 
-    return connect(mapStateToProps, mapDispatchToProps)(notAuthenticatedComponent);
+  function mapDispatchToProps(dispatch) {
+    return bindActionCreators(actionCreators, dispatch);
+  }
 
+  return connect(mapStateToProps, mapDispatchToProps)(AuthenticatedComponent);
 }
+
