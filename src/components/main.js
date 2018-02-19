c4b89be0aad5e35183c5254e85a973268f5e4271
Alex Gunning <alexandergunning@gmail.com>
2018-02-19 01:39:29 -0600

    removed heade from project

--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import BeerListContainer from '../containers/beerList'
-import Header from '../components/header'
 import AddBeerButton from '../components/addBeerButton.js'
 
 const Main = () => (
   <main>
-   <Header/>
    <AddBeerButton/>
    <BeerListContainer/>
   </main>
