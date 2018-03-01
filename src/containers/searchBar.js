22eecedaf84cfa8ce5f27a1817bae68c8f1f0484
Alex Gunning <alexandergunning@gmail.com>
2018-02-28 21:51:49 -0600

    Searchbar functionality

--- src/containers/searchBar.js
+++ src/containers/searchBar.js
@@ -5,7 +5,7 @@ import { Route } from 'react-router-dom'
 import * as _ from 'lodash'
 
 import { fetchBeerListIfNeeded } from '../actions/fetchBeerList'
-// var _ = require('lodash');
+import { updateSearch, deleteSearch } from '../actions/search'
 const Search = Input.Search;
 
 const parent =  {
@@ -13,20 +13,61 @@ const parent =  {
   justifyContent: "center",
 }
 
-function handleSearch (fetchBeer, history, value) {
+function handleSearch (fetchBeer, updateSearchString, history, value) {
   history.push(`?search=${value}`);
-  fetchBeer(value)
+  updateSearchString(value);
+  fetchBeer(value);
 }
 
+function handleClearSearch (fetchEmptyBeer, deleteSearchString, history, value) {
+  history.push('');
+  deleteSearchString();
+  fetchEmptyBeer();
+}
+//TODO figure out if we want to move some of this functionality to local state
+//also some of the actions the way we update where search is happeing and how it
+//interacts with the query string parameters feels repetetive
+
 let curriedHandleSearch = _.curry(handleSearch)
+let curriedClearSearch = _.curry(handleClearSearch)
+
+function parseQuery(queryString) {
+    var query = {};
+    var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+    for (var i = 0; i < pairs.length; i++) {
+        var pair = pairs[i].split('=');
+        query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+    }
+    return query;
+}
 
 class SearchBar extends Component {
 
   constructor(props) {
     super(props);
+    this.state = { value: '' };
   }
 
+  componentDidMount() {
+    let { fetchBeer, updateSearchString, location } = this.props;
+    let queryParams = parseQuery(location.search)
+    let currentSearch = queryParams.search;
+    if (currentSearch) {
+      this.setState({ value: currentSearch });
+      updateSearchString(currentSearch);
+      fetchBeer(currentSearch);
+    }
+  }
+
+  onChange = (e) => {
+    let { value } = e.target;
+    console.log(value);
+    this.setState({ value });
+  }
+
+  // defaultValue={this.props.search}
   render() {
+    let { search, fetchBeer, updateSearchString, deleteSearchString } = this.props;
     const temp = true;
     return (
       <div>
@@ -35,15 +76,21 @@ class SearchBar extends Component {
             <Search
               placeholder="Find Beers"
               style={{ width: "600px", margin: "20px" }}
-              onSearch={curriedHandleSearch(this.props.fetchBeer, history)}
               size="large"
+              onSearch={curriedHandleSearch(fetchBeer, updateSearchString, history)}
+              onChange={this.onChange}
+              value={this.state.value}
               enterButton
             />
             <div>
-              { temp ? <Button style={{ margin: "20px" } } icon="close">search term</Button> : <div></div> }
-            </div>
-            <br /><br />
-          </div>
+              { this.props.search ? <Button style={{ margin: "20px" } }
+                onClick={(value) => { this.setState({value: ""}); curriedClearSearch(fetchBeer, deleteSearchString, history)(value) }}
+                icon="close">
+                {this.props.search}</Button>
+                  : <div></div> }
+                </div>
+                <br /><br />
+              </div>
         ) } />
     </div>
     );
@@ -51,11 +98,13 @@ class SearchBar extends Component {
 }
 
 const mapStateToProps = (state) => ({
-
+  search: state.search.query
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchBeer: (search) => dispatch(fetchBeerListIfNeeded(search))
+  fetchBeer: (search) => dispatch(fetchBeerListIfNeeded(search)),
+  updateSearchString: (search) => dispatch(updateSearch(search)),
+  deleteSearchString: (search) => dispatch(deleteSearch(search))
 })
 
 export default connect(
--- src/reducers/search.js
+++ src/reducers/search.js
@@ -1,11 +1,11 @@
 import { UPDATE_SEARCH,DELETE_SEARCH } from "../actions/search";
 
-export function search(state = {search : null }, action) {
+export function search(state = {query : null }, action) {
   switch (action.type) {
     case UPDATE_SEARCH:
-      return { search : action.search };
+      return { query : action.search };
     case DELETE_SEARCH:
-      return { search : null };
+      return { query : null };
     default:
       return state
 }
