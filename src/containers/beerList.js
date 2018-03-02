491c0282ba46cf6e3d76aaf1e60bb2efe52927cb
Alex Gunning <alexandergunning@gmail.com>
2018-03-01 22:59:55 -0600

    allow half for stars on beerlist

--- src/containers/beerList.js
+++ src/containers/beerList.js
@@ -65,7 +65,7 @@ const Title = ({ beer, rating }) => (
     <div style={middleTitle}>
     </div>
     <div>
-      <Rate disabled defaultValue={rating} />
+      <Rate allowHalf disabled defaultValue={rating} />
     </div>
   </div>
 )
